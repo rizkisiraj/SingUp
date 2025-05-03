130e2a2e9e5f727e230cc4dcb942667374d79167
MChandraR <chandramadhan04@gmail.com>
2025-05-03 23:53:08 +0700

    Optimize UI : Leading icon added & Title

--- SingUP/View/WarmUpPage.swift
+++ SingUP/View/WarmUpPage.swift
@@ -10,29 +10,61 @@ struct WarmUpPage:View{
     
     var body: some View{
         VStack{
+            Spacer()
+            Text("Vocal Wram Up")
+                .font(.title.bold())
+                .frame(maxWidth : .infinity)
+                .padding(.vertical , 10)
+            
+            Text("Choose your warm up")
+                .font(.title3)
+                .padding(.bottom, 50)
+            
             GroupBox{
-                Text("Breath" )
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "lungs.fill")
+                        .font(.title)
+                        .padding()
+                    Text("Breath" )
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
             }
             .padding(.horizontal , 20)
             
             GroupBox{
-                Text("Humming")
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "person.wave.2.fill")
+                        .font(.title)
+                        .padding()
+                    Text("Humming")
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
             }
             .padding(.horizontal , 20)
             
             GroupBox{
-                Text("Lip Drill")
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "mouth.fill")
+                        .font(.title)
+                        .padding()
+                    Text("Lip Drill")
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
             }
             .padding(.horizontal , 20)
             
             GroupBox{
-                Text("Tongue Drill")
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "mouth")
+                        .font(.title)
+                        .padding()
+                    Text("Tongue Drill")
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
             }
             .padding(.horizontal , 20)
+            
+            Spacer()
         }
         .toolbarRole(.navigationStack)
         .toolbar{
