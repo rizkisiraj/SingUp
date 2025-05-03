724cab92089cd12a116b13651938e45295f0e4ac
MChandraR <chandramadhan04@gmail.com>
2025-05-03 23:42:15 +0700

    Optimize UI : Add leading icon and added title

--- SingUP/View/ExercisePage.swift
+++ SingUP/View/ExercisePage.swift
@@ -11,17 +11,48 @@ import SwiftUI
 struct ExercisePage: View {
     var body: some View {
         VStack{
+            Spacer()
+            Text("Vocal Exercise")
+                .font(.title.bold())
+                .frame(maxWidth : .infinity)
+                .padding(.vertical , 10)
+            
+            Text("Choose your exercise")
+                .font(.title3)
+                .padding(.bottom, 50)
+         
+            
             GroupBox{
-                Text("Sustain" )
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "chart.line.flattrend.xyaxis")
+                        .font(.title)
+                        .padding()
+
+                    Text("Sustain" )
+                        .bold(true)
+                        .font(.title)
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
+                
             }
             .padding(.horizontal , 20)
             
             GroupBox{
-                Text("Scale")
-                    .frame(maxWidth : .infinity, alignment : .leading)
+                HStack{
+                    Image(systemName: "arrow.down.left.arrow.up.right")
+                        .font(.title)
+                        .padding()
+                    
+                    Text("Scale")
+                        .bold(true)
+                        .font(.title)
+                        .frame(maxWidth : .infinity, alignment : .leading)
+                }
+               
             }
             .padding(.horizontal , 20)
+            
+            Spacer()
             
         }
         .toolbar{
