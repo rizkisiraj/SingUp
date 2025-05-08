220f64abbb2c7e0ba6efb9868c902884cc3c63d1
MChandraR <chandramadhan04@gmail.com>
2025-05-08 15:07:29 +0700

    Update home ui

--- SingUP/View/HomePage.swift
+++ SingUP/View/HomePage.swift
@@ -22,7 +22,7 @@ struct HomePage:View{
                 ScrollView{
                     VStack(alignment : .leading, spacing : 0){
                         
-                        Text("Your Vocal Range")
+                        Text("Your Vocal Range is :")
                             .font(.title2)
                             .multilineTextAlignment(.center)
                             .bold(true)
@@ -30,68 +30,109 @@ struct HomePage:View{
                             .padding(.bottom , 10)
                             .padding(.top, 50)
                         
-                        Image(systemName: "microphone.circle.fill")
-                            .multilineTextAlignment(.center)
-                            .font(.system(size : 100))
-                            .frame(maxWidth : .infinity, alignment : .center)
-                        
-                        
-                        Text(vocalRange.getVocalType(lowFreq: freq[0], highFreq: freq[1]))
-                            .font(.largeTitle)
-                            .bold(true)
-                            .frame(maxWidth : .infinity, alignment : .center)
-                            .padding(.bottom , 0)
-                            .padding(.top, 20)
-                        
-                        Text("\(getChordString(frequency : freq[0])) - \(getChordString(frequency : freq[1]))")
-                            .font(.title3)
-                            .bold(true)
-                            .frame(maxWidth : .infinity, alignment : .center)
-                            .padding(.bottom , 70)
-                            .padding(.top, 10)
-                        
                         GroupBox{
-                            HStack{
-                                Image(systemName : "waveform")
-                                    .font(.title)
-                                    .padding(.leading, 10)
-
-                                Text("Vocal WarmUp")
-                                    .multilineTextAlignment(.leading)
-                                    .font(.title2)
-                                    .bold(true)
-                                    .frame(maxWidth : .infinity, alignment : .leading)
-                                    .padding(.vertical, 20)
-                                    .padding(.horizontal, 10)
-                            }
+                            Text(vocalRange.getVocalType(lowFreq: freq[0], highFreq: freq[1]).uppercased())
+                                .foregroundStyle(Color("YellowVocal"))
+                                .font(.largeTitle.bold())
+                                .frame(maxWidth : .infinity, alignment : .center)
+                                .padding(.bottom , 0)
+                                .padding(.top, 20)
                             
-                            
+                            Text("\(getChordString(frequency : freq[0])) - \(getChordString(frequency : freq[1]))")
+                                .font(.title3)
+                                .bold(true)
+                                .frame(maxWidth : .infinity, alignment : .center)
+                                .padding(.bottom, 10)
                         }
-                        .onTapGesture {
-                            path.append("warmup")
-                        }
-                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius : 10)
+                                .fill(.white)
+                                .shadow(color : .gray.opacity(0.3), radius: 5,x : -2, y : 3)
+                        )
+                        .backgroundStyle(Color.clear)
+                        .frame(maxWidth : .infinity, alignment : .center)
+                        .padding(.horizontal, 50)
+                        .padding(.vertical, 20)
                         
-                        GroupBox{
+                        
+                        ScrollView(.horizontal){
                             HStack{
-                                Image(systemName : "music.microphone")
-                                    .font(.title)
-                                    .padding(.leading, 10)
+                                GroupBox{
+                                    VStack{
+                                       
+                                        Text("Vocal WarmUp")
+                                            .multilineTextAlignment(.center)
+                                            .font(.title.bold())
+                                            .frame(maxWidth : .infinity, alignment : .center)
+                                            .padding(.vertical, 20)
+                                            .padding(.horizontal, 10)
+                                        
+                                        Image("VocalWarmUp")
+                                            .resizable()
+                                            .frame(width : 180, height : 180)
+                                        
+                                        Text("Get your voice ready with quick and easy warm-ups to sing better, stronger, and safer.")
+                                            .multilineTextAlignment(.center)
+                                            .font(.caption)
+                                            .padding(.horizontal, 20)
+                                            .frame(maxWidth : .infinity, maxHeight : .infinity)
+                                        
+                                        
+                                        Button(
+                                            action : {
+                                                
+                                            }
+                                        ){
+                                            Text("Start")
+                                                .foregroundStyle(.white)
+                                                .frame(maxWidth : .infinity)
+                                        }
+                                        .padding(.vertical, 10)
+                                        .background(.black)
+                                        .cornerRadius(10)
+                                        .padding()
+                                    }
+                                    
+                                    
+                                }
+                                .backgroundStyle(Color.clear)
+                                .background(
+                                    RoundedRectangle(cornerRadius : 10)
+                                        .fill(Color("YellowWarmupCard").opacity(0.1))
+                                        .stroke(Color("YellowWarmupCard"), lineWidth : 1)
+                                )
+                                .frame(width : 300)
+                                .onTapGesture {
+                                    path.append("warmup")
+                                }
+                                .padding(.leading, 50)
                                 
-                                Text("Vocal Excercise")
-                                    .multilineTextAlignment(.leading)
-                                    .font(.title2)
-                                    .bold(true)
-                                    .frame(maxWidth : .infinity, alignment : .leading)
-                                    .padding(.vertical, 20)
-                                    .padding(.horizontal, 10)
+                                GroupBox{
+                                    VStack{
+                                        Image(systemName : "music.microphone")
+                                            .font(.title)
+                                            .padding(.leading, 10)
+                                        
+                                        Text("Vocal Excercise")
+                                            .multilineTextAlignment(.leading)
+                                            .font(.title2)
+                                            .bold(true)
+                                            .frame(maxWidth : .infinity, alignment : .leading)
+                                            .padding(.vertical, 20)
+                                            .padding(.horizontal, 10)
+                                    }
+                                    
+                                }
+                                .padding(.horizontal, 20)
+                                .onTapGesture {
+                                    path.append("exercise")
+                                }
                             }
                             
                         }
-                        .padding(.horizontal, 20)
-                        .onTapGesture {
-                            path.append("exercise")
-                        }
+                        .padding(.top, 30)
+                        
+                        
                         
                         Button(action :  {
                             if let user = userProfile.first{
